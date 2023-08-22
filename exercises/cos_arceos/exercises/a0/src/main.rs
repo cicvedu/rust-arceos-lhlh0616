d888ba351ff7a7b687b194c630bc44bf19a1896f
lhlh0616 <141699370+lhlh0616@users.noreply.github.com>
2023-08-22 12:40:00 +0800

    Update main.rs

--- exercises/cos_arceos/exercises/a0/src/main.rs
+++ exercises/cos_arceos/exercises/a0/src/main.rs
@@ -8,23 +8,12 @@ use axstd::println;
 
 // TODO: Implement macro println_prefix.
 
-#[macro_export]
-macro_rules! println_prefix {
-    ($prefix:expr,) => {
-        println!("{}", $prefix);
-    };
-    ($prefix:expr, $fmt:expr, $($arg:tt)*) => {
-        println!("{}{}", $prefix, format!($fmt, $($arg)*));
-    };
-}
-
-
 #[cfg(feature = "axstd")]
 use axstd::println_prefix;
 
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main() {
-    println!("Hello, world!");
+    println!("Hello,{}","world!");
 
     let times = 2;
     println_prefix!("Stdout: ", "Hello, world![{}]", times);
