d5503fbd70b8a4e90a724404c4e95f24c996fb2d
lhlh0616 <141699370+lhlh0616@users.noreply.github.com>
2023-08-24 23:02:17 +0800

    Update simple.rs

--- exercises/cos_arceos/crates/scheduler/src/simple.rs
+++ exercises/cos_arceos/crates/scheduler/src/simple.rs
@@ -1,22 +1,18 @@
-use alloc::{collections::VecDeque, sync::Arc};
-use core::ops::Deref;
+use std::collections::LinkedList;
+use std::sync::{Arc, Mutex};
+use std::ops::Deref;
 
-use crate::BaseScheduler;
-
-/// A task wrapper for the [`SimpleScheduler`].
 pub struct SimpleTask<T> {
     inner: T,
 }
 
 impl<T> SimpleTask<T> {
-    /// Creates a new [`SimpleTask`] from the inner task struct.
     pub const fn new(inner: T) -> Self {
         Self {
             inner,
         }
     }
 
-    /// Returns a reference to the inner task struct.
     pub const fn inner(&self) -> &T {
         &self.inner
     }
@@ -30,62 +26,37 @@ impl<T> Deref for SimpleTask<T> {
     }
 }
 
-/// A simple scheduler.
-///
-/// When a task is added to the scheduler, it's placed at the end of the ready
-/// queue. When picking the next task to run, the head of the ready queue is
-/// taken.
-///
-/// As it's a cooperative scheduler, it does nothing when the timer tick occurs.
-///
 pub struct SimpleScheduler<T> {
-    ready_queue: VecDeque<Arc<SimpleTask<T>>>,
+    ready_queue: Arc<Mutex<LinkedList<SimpleTask<T>>>>,
 }
 
 impl<T> SimpleScheduler<T> {
-    /// Creates a new empty [`SimpleScheduler`].
-    pub const fn new() -> Self {
-        Self {
-            ready_queue: VecDeque::new(),
+    pub fn new() -> Self {
+        SimpleScheduler {
+            ready_queue: Arc::new(Mutex::new(LinkedList::new())),
         }
     }
-    /// get the name of scheduler
-    pub fn scheduler_name() -> &'static str {
-        "Simple"
-    }
-}
 
-impl<T> BaseScheduler for SimpleScheduler<T> {
-    type SchedItem = Arc<SimpleTask<T>>;
-
-    fn init(&mut self) {}
-
-    fn add_task(&mut self, task: Self::SchedItem) {
-        trace!("######### add_task");
-        self.ready_queue.push_back(task);
+    pub fn add_task(&mut self, task: SimpleTask<T>) {
+        let mut queue = self.ready_queue.lock().unwrap();
+        queue.push_back(task);
     }
 
-    fn remove_task(&mut self, task: &Self::SchedItem) -> Option<Self::SchedItem> {
-        trace!("######### remove_task");
-        self.ready_queue
-            .iter()
-            .position(|t| Arc::ptr_eq(t, task))
-            .and_then(|idx| self.ready_queue.remove(idx))
+    pub fn remove_task(&mut self, task: &SimpleTask<T>) -> Option<SimpleTask<T>> {
+        let mut queue = self.ready_queue.lock().unwrap();
+        let index = queue.iter().position(|t| t.inner() == task.inner()).unwrap();
+        if let Some(index) = index {
+            let removed_task = queue.remove(index);
+            return Some(removed_task);
+        }
+        None
     }
 
-    fn pick_next_task(&mut self) -> Option<Self::SchedItem> {
-        self.ready_queue.pop_front()
-    }
-
-    fn put_prev_task(&mut self, prev: Self::SchedItem, _preempt: bool) {
-        self.ready_queue.push_back(prev);
-    }
-
-    fn task_tick(&mut self, _current: &Self::SchedItem) -> bool {
-        false // no reschedule
-    }
-
-    fn set_priority(&mut self, _task: &Self::SchedItem, _prio: isize) -> bool {
-        false
+    pub fn pick_next_task(&mut self) -> Option<SimpleTask<T>> {
+        let mut queue = self.ready_queue.lock().unwrap();
+        if let Some(task) = queue.pop_front() {
+            return Some(task);
+        }
+        None
     }
 }
