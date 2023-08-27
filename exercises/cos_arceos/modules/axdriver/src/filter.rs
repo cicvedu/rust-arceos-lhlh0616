87d0264073793d160b9ab8d049ebe35ab6b2ab36
lhlh0616 <141699370+lhlh0616@users.noreply.github.com>
2023-08-27 23:36:28 +0800

    Update filter.rs

--- exercises/cos_arceos/modules/axdriver/src/filter.rs
+++ exercises/cos_arceos/modules/axdriver/src/filter.rs
@@ -1,4 +1,5 @@
 use cfg_if::cfg_if;
+
 // use driver_net::NetDriverOps;
 // use driver_virtio::VirtIoNetDev;
 // use driver_virtio::Transport;
@@ -24,55 +25,56 @@ cfg_if! {
         use crate::virtio::VirtIoHalImpl;
 
         impl BaseDriverOps for NetFilter<driver_virtio::VirtIoNetDev<VirtIoHalImpl, VirtIoTransport, 64>> {
+            fn device_type(&self) -> driver_common::DeviceType {
+                driver_common::DeviceType::Net
+            }
+
+            fn device_name(&self) -> &str {
+                "net"
+            }
+        }
+
+       impl<Driver: VirtIoNetDev<VirtIoHalImpl, VirtIoTransport, 64>> NetDriverOps for NetFilter<Driver>{
             #[inline]
             fn mac_address(&self) -> driver_net::EthernetAddress {
                 self.inner.mac_address()
             }
 
-            #[inline]
             fn can_transmit(&self) -> bool {
                 self.inner.can_transmit()
             }
 
-            #[inline]
             fn can_receive(&self) -> bool {
                 self.inner.can_receive()
             }
 
-            #[inline]
             fn rx_queue_size(&self) -> usize {
                 self.inner.rx_queue_size()
             }
 
-            #[inline]
             fn tx_queue_size(&self) -> usize {
                 self.inner.tx_queue_size()
             }
 
-            #[inline]
             fn recycle_rx_buffer(&mut self, rx_buf: driver_net::NetBufPtr) -> driver_common::DevResult {
                 self.inner.recycle_rx_buffer(rx_buf)
             }
 
-            #[inline]
             fn recycle_tx_buffers(&mut self) -> driver_common::DevResult {
                 self.inner.recycle_tx_buffers()
             }
 
-            #[inline]
             fn transmit(&mut self, tx_buf: driver_net::NetBufPtr) -> driver_common::DevResult {
                 log::warn!("Filter: transmit len [{}]", tx_buf.packet_len());
                 self.inner.transmit(tx_buf)
             }
 
-            #[inline]
             fn receive(&mut self) -> driver_common::DevResult<driver_net::NetBufPtr> {
                 let ret = self.inner.receive()?;
                 log::warn!("Filter: receive len[{:?}]", ret.packet_len());
                 Ok(ret)
             }
 
-            #[inline]
             fn alloc_tx_buffer(&mut self, size: usize) -> driver_common::DevResult<driver_net::NetBufPtr> {
                 self.inner.alloc_tx_buffer(size)
             }
